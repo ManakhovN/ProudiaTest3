dae98785cfc2a06a9c958f609e62f7299d3d733b
Надир <nadirmanakhov@gmail.com>
2019-04-12 10:29:33 +0300

    Job done.

---
+++ Assets/Scripts/SphereTrackableEventHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereTrackableEventHandler : DefaultTrackableEventHandler
+{
+    public static event Action<SphereTrackableEventHandler> OnInstantiate;
+    public event Action OnFound = delegate { };
+    public event Action OnLost = delegate { };
+    
+    protected override void Start()
+    {
+        base.Start();
+        if (!gameObject.name.Equals("UserDefinedTarget"))
+        OnInstantiate.Invoke(this);
+    }
+
+    protected override void OnTrackingFound()
+    {        
+        base.OnTrackingFound();
+        var meshRenderer = GetComponentInChildren<MeshRenderer>();
+        if (meshRenderer != null)
+            meshRenderer.material.color = new Color(UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f));
+        OnFound();
+    }
+
+    protected override void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+        OnLost();
+    }
+}
---
+++ Assets/Scripts/SpheresController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpheresController : MonoBehaviour {
+    List<SphereTrackableEventHandler> spheresTrackers = new List<SphereTrackableEventHandler>();
+    List<SphereTrackableEventHandler> activeSpheres = new List<SphereTrackableEventHandler>();
+	void Awake () {
+        SphereTrackableEventHandler.OnInstantiate += SphereAdded;	
+	}
+
+    private void SphereAdded(SphereTrackableEventHandler sphere)
+    {
+        spheresTrackers.Add(sphere);
+        sphere.OnFound += () =>activeSpheres.Add(sphere);
+        sphere.OnLost += () => activeSpheres.Remove(sphere);
+    }
+
+    private float timer = 0f;
+    [SerializeField] float speed = 10f;
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 2f)
+        {
+            timer -= 2f;
+            MoveSpheres();
+        }
+    }
+
+    private void MoveSpheres() {
+        for (int i = 0; i < activeSpheres.Count; i++)
+        {
+            Transform go = activeSpheres[i].transform.GetChild(0);
+            go.SetParent(activeSpheres[(i + 1) % activeSpheres.Count].transform);
+            StartCoroutine(MovePositionTo(go, Vector3.zero, speed));
+        }
+    }
+
+    IEnumerator MovePositionTo(Transform obj, Vector3 destination, float speed)
+    {
+        while (!obj.localPosition.Equals(destination))
+        {
+            obj.localPosition = Vector3.MoveTowards(obj.localPosition, destination, speed * Time.deltaTime);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
